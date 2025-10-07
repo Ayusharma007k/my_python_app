48079cad28561474a2ada45fbf98cf244f181cd0
Ayush Sharma <ayush2001khandal@gmail.com>
2025-10-07 17:22:16 +0530

    update workflow

---
+++ terraformv3/examples/complete/container_platform/variables.tf
@@ -0,0 +1,11 @@
+variable "image_tag" {
+  description = "Tag of the Docker image to deploy"
+  type        = string
+  default     = "latest"
+}
+
+variable "env" {
+  description = "Environment name (dev, test, prod)"
+  type        = string
+  default     = "dev"
+}
