337469fa6adc2b73228218553b0c49accaa3eacd
Ayush Sharma <ayush2001khandal@gmail.com>
2025-10-08 14:29:49 +0530

    letest updated files

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ayush-rg"
-    storage_account_name = "ayushtfstatestorexyz"
-    container_name       = "tfstate"
-    key                  = "singleapp.tfstate"
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.app.default_hostname
-}
